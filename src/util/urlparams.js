4038162f475e75b9dab59bc445c19dfb510029bc
박건우 <woorl201362@gmail.com>
2025-02-18 12:17:04 +0900

    urlparams / util module 추가
    urlparams / util module 추가

---
+++ src/util/urlparams.js
@@ -0,0 +1,13 @@
+import { useLocation } from "react-router-dom";
+
+/* 
+url의 쿼리 파라미터 값들을 한 번에 객체로 반환
+(예시)
+http://localhost:3000?accessToken=xyz123&nickname=홍길동
+{ accessToken: "xyz123", nickname: "홍길동" }
+*/
+const useQueryParams = () => {
+  return Object.fromEntries(new URLSearchParams(useLocation().search));
+};
+
+export default useQueryParams;
